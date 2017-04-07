479355c731e60716acb532253e61852857ca7794
Yosuke ONOUE <onoue@likr-lab.com>
2017-04-07 11:25:05 +0900

    add matrix.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate gleam;
 extern crate emscripten_sys;
 
+mod matrix;
+
 use gleam::gl;
 use gleam::gl::{GLenum, GLuint};
 use emscripten_sys::{
@@ -12,9 +14,16 @@ use emscripten_sys::{
     emscripten_get_element_css_size,
     EmscriptenWebGLContextAttributes,
 };
+use matrix::{
+    rotate_x,
+    rotate_y,
+    viewing_matrix,
+    perspective_matrix,
+    matmul,
+    Matrix44,
+};
 
 type GlPtr = std::rc::Rc<gl::Gl>;
-type GlMatrix = [f32; 16];
 
 #[repr(C)]
 struct Context {
@@ -22,8 +31,8 @@ struct Context {
     program: GLuint,
     buffer: GLuint,
     theta: f32,
-    mv_matrix: GlMatrix,
-    p_matrix: GlMatrix,
+    camera: Matrix44,
+    p_matrix: Matrix44,
     width: u32,
     height: u32,
 }
@@ -89,126 +98,6 @@ fn init_buffer(gl: &GlPtr, program: GLuint) -> Option<GLuint> {
     Some(array)
 }
 
-fn zeros() -> GlMatrix {
-    [0f32; 16]
-}
-
-fn identity() -> GlMatrix {
-    let mut matrix = zeros();
-    matrix[0] = 1.0;
-    matrix[5] = 1.0;
-    matrix[10] = 1.0;
-    matrix[15] = 1.0;
-    matrix
-}
-
-fn rotate_x(theta: f32) -> GlMatrix {
-    let mut matrix = identity();
-    matrix[5] = theta.cos();
-    matrix[6] = theta.sin();
-    matrix[9] = -theta.sin();
-    matrix[10] = theta.cos();
-    matrix
-}
-
-fn rotate_y(theta: f32) -> GlMatrix {
-    let mut matrix = identity();
-    matrix[0] = theta.cos();
-    matrix[2] = theta.sin();
-    matrix[8] = -theta.sin();
-    matrix[10] = theta.cos();
-    matrix
-}
-
-fn translate(x: f32, y: f32, z: f32) -> GlMatrix {
-    let mut matrix = identity();
-    matrix[12] = x;
-    matrix[13] = y;
-    matrix[14] = z;
-    matrix
-}
-
-fn cross(v1: [f32; 3], v2: [f32; 3]) -> [f32; 3] {
-    [
-        v1[1] * v2[2] - v1[2] * v2[1],
-        v1[2] * v2[0] - v1[0] * v2[2],
-        v1[0] * v2[1] - v1[1] * v2[0],
-    ]
-}
-
-fn normalize(v1: [f32; 3]) -> [f32; 3] {
-    let sum = v1[0] + v1[1] + v1[2];
-    [
-        v1[0] / sum,
-        v1[1] / sum,
-        v1[2] / sum,
-    ]
-}
-
-fn viewing_matrix(eye: [f32; 3], up: [f32; 3], target: [f32; 3]) -> GlMatrix {
-    let d = [
-        target[0] - eye[0],
-        target[1] - eye[1],
-        target[2] - eye[2],
-    ];
-    let r = cross(d, up);
-    let f = cross(r, d);
-    let d = normalize(d);
-    let r = normalize(r);
-    let f = normalize(f);
-    let mut matrix = identity();
-    matrix[0] = r[0];
-    matrix[4] = r[1];
-    matrix[8] = r[2];
-    matrix[1] = f[0];
-    matrix[5] = f[1];
-    matrix[9] = f[2];
-    matrix[2] = -d[0];
-    matrix[6] = -d[1];
-    matrix[10] = -d[2];
-    matmul(matrix, translate(-eye[0], -eye[1], -eye[2]))
-}
-
-fn orthogonal_matrix(left: f32, right: f32, top: f32, bottom: f32, near: f32, far: f32) -> GlMatrix {
-    let mut matrix = zeros();
-    let w = right - left;
-    let x = right + left;
-    let h = top - bottom;
-    let y = top + bottom;
-    let d = far - near;
-    let z = far + near;
-    matrix[0] = 2.0 / w;
-    matrix[5] = 2.0 / h;
-    matrix[10] = -1.0 / d;
-    matrix[12] = -x / w;
-    matrix[13] = -y / h;
-    matrix[14] = -z / d;
-    matrix[15] = 1.0;
-    matrix
-}
-
-fn perspective_matrix(fov: f32, aspect: f32, near: f32, far: f32) -> GlMatrix {
-    let mut matrix = zeros();
-    matrix[0] = 1.0 / fov.tan() / aspect;
-    matrix[5] = 1.0 / fov.tan();
-    matrix[10] = -(far + near) / (far - near);
-    matrix[11] = -1.0;
-    matrix[14] = -2.0 * far * near / (far - near);
-    matrix
-}
-
-fn matmul(a: GlMatrix, b: GlMatrix) -> GlMatrix {
-    let mut c = zeros();
-    for i in 0..4 {
-        for j in 0..4 {
-            for k in 0..4 {
-                c[i * 4 + j] += a[i * 4 + k] * b[k * 4 + j];
-            }
-        }
-    }
-    c
-}
-
 impl Context {
     fn new(gl: GlPtr) -> Context {
         let v_shader = load_shader(&gl, gl::VERTEX_SHADER, VS_SRC).unwrap();
@@ -231,7 +120,7 @@ impl Context {
             program: program,
             buffer: buffer,
             theta: 0.0,
-            mv_matrix: identity(),
+            camera: viewing_matrix([0.0, 0.0, 200.0], [0.0, 1.0, 0.0], [0.0, 0.0, 0.0]),
             p_matrix: perspective_matrix((45.0 as f32).to_radians(), width as f32 / height as f32, 0.001, 1000.0),
             width: width,
             height: height,
@@ -244,7 +133,8 @@ impl Context {
         gl.clear(gl::COLOR_BUFFER_BIT);
         gl.use_program(self.program);
         let mv_location = gl.get_uniform_location(self.program, "uMVMatrix");
-        gl.uniform_matrix_4fv(mv_location, false, &self.mv_matrix);
+        let mv_matrix = matmul(matmul(rotate_x(self.theta), rotate_y(self.theta)), self.camera);
+        gl.uniform_matrix_4fv(mv_location, false, &mv_matrix);
         let p_location = gl.get_uniform_location(self.program, "uPMatrix");
         gl.uniform_matrix_4fv(p_location, false, &self.p_matrix);
         gl.bind_vertex_array(self.buffer);
@@ -263,12 +153,7 @@ fn get_canvas_size() -> (u32, u32) {
 }
 
 fn step(ctx: &mut Context) {
-    let (width, height) = get_canvas_size();
-    ctx.width = width;
-    ctx.height = height;
-    let camera = viewing_matrix([0.0, 0.0, 200.0], [0.0, 1.0, 0.0], [0.0, 0.0, 0.0]);
     ctx.theta += 0.01;
-    ctx.mv_matrix = matmul(matmul(rotate_x(ctx.theta), rotate_y(ctx.theta)), camera);
     ctx.draw();
 }
 
---
+++ src/matrix.rs
@@ -0,0 +1,122 @@
+pub type Vec3 = [f32; 3];
+pub type Matrix44 = [f32; 16];
+
+pub fn zeros() -> Matrix44 {
+    [0f32; 16]
+}
+
+pub fn identity() -> Matrix44 {
+    let mut matrix = zeros();
+    matrix[0] = 1.0;
+    matrix[5] = 1.0;
+    matrix[10] = 1.0;
+    matrix[15] = 1.0;
+    matrix
+}
+
+pub fn rotate_x(theta: f32) -> Matrix44 {
+    let mut matrix = identity();
+    matrix[5] = theta.cos();
+    matrix[6] = theta.sin();
+    matrix[9] = -theta.sin();
+    matrix[10] = theta.cos();
+    matrix
+}
+
+pub fn rotate_y(theta: f32) -> Matrix44 {
+    let mut matrix = identity();
+    matrix[0] = theta.cos();
+    matrix[2] = theta.sin();
+    matrix[8] = -theta.sin();
+    matrix[10] = theta.cos();
+    matrix
+}
+
+pub fn translate(x: f32, y: f32, z: f32) -> Matrix44 {
+    let mut matrix = identity();
+    matrix[12] = x;
+    matrix[13] = y;
+    matrix[14] = z;
+    matrix
+}
+
+pub fn cross(v1: Vec3, v2: Vec3) -> Vec3 {
+    [
+        v1[1] * v2[2] - v1[2] * v2[1],
+        v1[2] * v2[0] - v1[0] * v2[2],
+        v1[0] * v2[1] - v1[1] * v2[0],
+    ]
+}
+
+pub fn normalize(v: Vec3) -> Vec3 {
+    let sum = (v[0] * v[0] + v[1] * v[1] + v[2] * v[2]).sqrt();
+    [
+        v[0] / sum,
+        v[1] / sum,
+        v[2] / sum,
+    ]
+}
+
+pub fn viewing_matrix(eye: Vec3, up: Vec3, target: Vec3) -> Matrix44 {
+    let d = [
+        target[0] - eye[0],
+        target[1] - eye[1],
+        target[2] - eye[2],
+    ];
+    let r = cross(up, d);
+    let f = cross(d, r);
+    let d = normalize(d);
+    let r = normalize(r);
+    let f = normalize(f);
+    let mut matrix = identity();
+    matrix[0] = r[0];
+    matrix[4] = r[1];
+    matrix[8] = r[2];
+    matrix[1] = f[0];
+    matrix[5] = f[1];
+    matrix[9] = f[2];
+    matrix[2] = -d[0];
+    matrix[6] = -d[1];
+    matrix[10] = -d[2];
+    matmul(translate(-eye[0], -eye[1], -eye[2]), matrix)
+}
+
+pub fn orthogonal_matrix(left: f32, right: f32, top: f32, bottom: f32, near: f32, far: f32) -> Matrix44 {
+    let mut matrix = zeros();
+    let w = right - left;
+    let x = right + left;
+    let h = top - bottom;
+    let y = top + bottom;
+    let d = far - near;
+    let z = far + near;
+    matrix[0] = 2.0 / w;
+    matrix[5] = 2.0 / h;
+    matrix[10] = -1.0 / d;
+    matrix[12] = -x / w;
+    matrix[13] = -y / h;
+    matrix[14] = -z / d;
+    matrix[15] = 1.0;
+    matrix
+}
+
+pub fn perspective_matrix(fov: f32, aspect: f32, near: f32, far: f32) -> Matrix44 {
+    let mut matrix = zeros();
+    matrix[0] = 1.0 / fov.tan() / aspect;
+    matrix[5] = 1.0 / fov.tan();
+    matrix[10] = -(far + near) / (far - near);
+    matrix[11] = -1.0;
+    matrix[14] = -2.0 * far * near / (far - near);
+    matrix
+}
+
+pub fn matmul(a: Matrix44, b: Matrix44) -> Matrix44 {
+    let mut c = zeros();
+    for i in 0..4 {
+        for j in 0..4 {
+            for k in 0..4 {
+                c[i * 4 + j] += a[i * 4 + k] * b[k * 4 + j];
+            }
+        }
+    }
+    c
+}
